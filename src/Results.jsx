846a4f2e14cc32b7fe72b104435d65d98b13409a
saraazizhere <saraaziz180903@gmail.com>
2024-08-06 02:03:02 +0500

    added results

--- src/Results.jsx
+++ src/Results.jsx
@@ -3,22 +3,30 @@ import Pet from "./Pet";
 const Results = ({ pets }) => {
     return (
         <div className="search">
-            {!pets.length ? (
-                <h1>No Pets Found</h1>
-            ) : (
-                pets.map((pet) => {
-                    return (
-                        <Pet
-                            animal={pet.animal}
-                            name={pet.name}
-                            breed={pet.breed}
-                            images={pet.images}
-                            location={`${pet.city}, ${pet.state}`}
-                            key={pet.id}
-                        />
-                    );
-                })
-            )}
+            {
+                // using conditional rendering
+                // condition ? trueExpression : falseExpression
+                !pets.length ? (
+                    <h1>No Pets Found</h1>
+                ) : (
+                    pets.map((pet) => {
+                        return (
+                            <Pet
+                                name={pet.name}
+                                animal={pet.animal}
+                                breed={pet.breed}
+                                images={pet.images}
+                                location={`${pet.city}, ${pet.state}`}
+                                key={pet.id}
+                                /* instead of passing all these attributes we can also do:
+                                <Pet {...pet} key={pet.id} /> 
+                                this uses spread operator (the three dots), it will work the same
+                                but it's not a good approach as it is unclear about what you're passing*/
+                            />
+                        );
+                    })
+                )
+            }
         </div>
     );
 };
